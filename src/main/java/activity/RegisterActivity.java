4b6e719be4a54e2dc615f9dac0b44f90a568c8a6
Aji Fauzi P <ajifauzi949@gmail.com>
2025-01-16 09:50:25 +0700

    add locator and methods for RegisterActivity

---
+++ src/main/java/activity/RegisterActivity.java
@@ -0,0 +1,44 @@
+package activity;
+
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.By;
+
+public class RegisterActivity {
+    AppiumDriver driver;
+
+    public RegisterActivity(AppiumDriver driver) {
+        this.driver = driver;
+    }
+
+    // locator
+    By usernameEt = By.xpath("//android.widget.EditText[@text='Username']");
+    By emailEt = By.xpath("//android.widget.EditText[@text='Email']");
+    By passwordEt = By.xpath("//android.widget.EditText[@text='Password']");
+    By confirmPassEt = By.xpath("//android.widget.EditText[@text='Konfirmasi Password']");
+    By registerBtn = By.id("com.kazakimaru.ch04_ajifauzipangestu:id/btn_daftar");
+
+    //methods
+    public void clickRegister() {
+        driver.findElement(registerBtn).click();
+    }
+
+    public void setUsername(String username) {
+        driver.findElement(usernameEt).clear();
+        driver.findElement(usernameEt).sendKeys(username);
+    }
+
+    public void setEmail(String email) {
+        driver.findElement(emailEt).clear();
+        driver.findElement(emailEt).sendKeys(email);
+    }
+
+    public void setPassword(String password) {
+        driver.findElement(passwordEt).clear();
+        driver.findElement(passwordEt).sendKeys(password);
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        driver.findElement(confirmPassEt).clear();
+        driver.findElement(confirmPassEt).sendKeys(confirmPassword);
+    }
+}
